a8d678ac8a20b7519a744fdec97ad2e5753eef18
Ryan Chiechi <rchiech@ncsu.edu>
2023-12-16 08:37:59 -0500

    Encoding latex

--- src/bibtex/write.py
+++ src/bibtex/write.py
@@ -9,13 +9,11 @@ import interact
 logger = getlogger(__name__)
 
 layers = [
-    m.RemoveEnclosingMiddleware(True), # Remove enclosures to parse dates correctly
-    m.MonthIntMiddleware(True), # Months should be represented as int (0-12)
-    m.LatexDecodingMiddleware(True)
+    m.LatexEncodingMiddleware(True)
 ]
 
 def write_bib(library, db):
     if os.path.exists(db):
         if not interact.overwrite(os.path.basename(db)):
             return
-    bibtexparser.write_file(db, library)
+    bibtexparser.write_file(db, library, append_middleware=layers)
