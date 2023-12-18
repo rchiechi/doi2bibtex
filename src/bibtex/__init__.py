477e77230442960a062af0ed6d24c223e3b4450e
Ryan Chiechi <rchiech@ncsu.edu>
2023-12-18 07:53:55 -0500

    Move writers to output module

--- src/bibtex/__init__.py
+++ src/bibtex/__init__.py
@@ -1,14 +1,10 @@
 from .read import load_bib
 from .dedupe import dedupe_bib_library
-from .write import write_bib
 from .clean import EntryCleaner
 from .replace import replace_doi_in_file
 
 def read(fn, dedupe=False):
     return load_bib(fn, dedupe)
-
-def write(library, fn):
-    return write_bib(library, fn)
 
 def dedupe(library):
     return dedupe_bib_library(library)
---
+++ src/output/__init__.py
@@ -0,0 +1,4 @@
+from .bibtexdb import write_bib
+
+def bibtexdb(library, fn):
+    return write_bib(library, fn)
