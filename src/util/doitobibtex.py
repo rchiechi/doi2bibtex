560a72e5ec18f0f8da6b28243f7a0755a2291b45
Ryan Chiechi <rchiech@ncsu.edu>
2024-08-21 07:20:50 -0400

    Try go guess page from DOI for e.g. Wiley journals

--- src/util/doitobibtex.py
+++ src/util/doitobibtex.py
@@ -1,10 +1,11 @@
 import urllib.request
 from urllib.error import HTTPError
 import http
+import re
 from .getlogger import return_logger
 
 BASE_URL = 'https://dx.doi.org/'
-
+PAGERE = re.compile(r'\d+\.\d+/\D+20(\d+)')
 logger = return_logger(__name__)
 
 def get_bibtex_from_url(doi):
@@ -24,4 +25,17 @@ def get_bibtex_from_url(doi):
             logger.error(f"Error {err.code} while fetching {url}")
     except http.client.InvalidURL:
         logger.error(f"'{doi}' is not a valid url")
+    if 'pages' not in bibtex.lower():
+        bibtex = add_pages(bibtex, doi)
     return bibtex
+
+def add_pages(bibtex, doi):
+    logger.warning("Guessing page from DOI:%s", doi)
+    m = re.match(PAGERE, doi)
+    try:
+        page = m.group(1)
+    except IndexError:
+        return bibtex
+    _biblist = bibtex.split(',')
+    _biblist.insert(-1, f'pages={page}')
+    return ','.join(_biblist)
